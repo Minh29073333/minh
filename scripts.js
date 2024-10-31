34937ab0cca1d713d7eddea8ed5773d12ed5a87a
Minh29073333 <dntm2907@gmail.com>
2024-10-31 21:08:10 +0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector("header");
+    header.style.transition = "opacity 1s";
+    header.style.opacity = "1";
+});
+
+function showStory() {
+    const storySection = document.getElementById("story");
+    storySection.scrollIntoView({ behavior: "smooth" });
+}
