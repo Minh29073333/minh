653b183f934a82a33884acd241cf9c741c73c42a
Minh29073333 <dntm2907@gmail.com>
2024-10-31 21:33:34 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,12 +9,8 @@ function showStory() {
     storySection.scrollIntoView({ behavior: "smooth" });
 }
 document.addEventListener("DOMContentLoaded", function() {
-    const header = document.querySelector("header");
-    header.style.transition = "opacity 1s";
-    header.style.opacity = "1";
-
     // Ngày gặp nhau (thay bằng ngày thực tế)
-    const startDate = new Date("2023-12-31"); // Định dạng YYYY-MM-DD
+    const startDate = new Date("2022-01-01"); // Định dạng YYYY-MM-DD
 
     // Hàm tính số ngày từ ngày gặp nhau đến hiện tại
     function calculateDaysTogether() {
@@ -24,6 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
         return daysTogether;
     }
 
-    // Hiển thị số ngày lên trang
+    // Hiển thị số ngày lên trái tim toàn màn hình
     document.getElementById("days-counter").textContent = calculateDaysTogether();
+
+    // Ẩn trái tim sau 8 giây
+    setTimeout(() => {
+        document.getElementById("full-screen-heart").classList.add("hidden");
+    }, 8000);
 });
